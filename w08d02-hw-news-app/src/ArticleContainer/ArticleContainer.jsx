26612ea98bd5166a4350903737a021aa3b4bff3b
Destiny <dshillis1@gmail.com>
2019-05-07 16:41:04 -0600

    Acticles populate

--- w08d02-hw-news-app/src/ArticleContainer/ArticleContainer.jsx
+++ w08d02-hw-news-app/src/ArticleContainer/ArticleContainer.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import Article from './Article/Article';
 
 class ArticleContainer extends Component {
     constructor(){
         super();
         this.state = {
-            articles: ['testing', 'one', 'two']
+            articles: [],
         }
     }
     componentDidMount(){
@@ -16,30 +15,28 @@ class ArticleContainer extends Component {
         const searchURL = 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=e47262b0f8134548a5f8414e52d27d55';
         const result = await fetch(searchURL);
         const parsedResult = await result.json();
+        const articlesArray = parsedResult.articles;
+        const articlesShow = articlesArray.map((article) => {
+            return (
+                <div>
+                    <h4>{ article.title }</h4>
+                    <p>{ article.description } </p>
+                    <a target="_blank" href={ article.url }><button className="btn btn-secondary">Read More</button></a>
+                    <hr />
+                </div>
+            )
+        })
         this.setState({
-            articles: parsedResult
-        });
-        console.log(this.state.articles)
+            articles: articlesShow
+        })
     };
     render(){
-        // const articlesList = this.state.articles.map((article) => {
-        //     return(
-        //         <div>Stuff</div>
-        //         // <Article article={ article } />
-        //     )
-        // })
         return(
             <div className="container-fluid">
             <div className="row justify-content-center">
-              <div className="col-sm-6">
-                <div className="center-text">  
+              <div className="col-sm-12">
             <h1>Articles</h1>
-            <div key={ this.state.articles.title}>
-            {
-                JSON.stringify(this.state.articles.articles)
-            }
-            </div>
-                </div>
+            { this.state.articles }
               </div>
             </div>
           </div>
