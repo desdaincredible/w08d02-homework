ccdf755aff34e2cf87d5a36c3448775fcd1b7558
Destiny <dshillis1@gmail.com>
2019-05-06 22:39:31 -0600

    Close to getting article to work

--- w08d02-hw-news-app/src/ArticleContainer/ArticleContainer.jsx
+++ w08d02-hw-news-app/src/ArticleContainer/ArticleContainer.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import Article from './Article/Article';
+import { async } from 'q';
 
 class ArticleContainer extends Component {
+    constructor(){
+        super();
+        this.state = {
+            articles: ['testing', 'one', 'two']
+        }
+    }
+    componentDidMount(){
+        this.getArticles();
+    }
+    getArticles = async () => {
+        // API call
+        const searchURL = 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=e47262b0f8134548a5f8414e52d27d55';
+        const result = await fetch(searchURL);
+        const parsedResult = await result.json();
+        this.setState({
+            articles: parsedResult
+        });
+        console.log(this.state.articles)
+    };
     render(){
+        const articlesList = this.state.articles.map((article) => {
+            return(
+                <Article article={ article } />
+            )
+        })
         return(
             <div className="container-fluid">
             <div className="row justify-content-center">
               <div className="col-sm-6">
                 <div className="center-text">  
-            <h1>Article Container</h1>
+            <h1>Articles</h1>
+            { articlesList }
                 </div>
               </div>
             </div>
